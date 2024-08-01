a51590c306f4ff32cf1bf1fbf46fc18bd2fae296
HPNL <hpnl.informatique@elsan.care>
2024-08-01 07:49:00 +0000

    correct bug

--- views.py
+++ views.py
@@ -163,7 +163,7 @@ def gen_page_sys(p_titre_slugify):
 	page.c_includ_rp = get_data_value('includ-right-panel')
 	if page.c_card_mp == "non":
 		page.c_card_mp = False
-	elif page.p_c_card_mp == "oui":
+	elif page.c_card_mp == "oui":
 		page.c_card_mp = True
 	else :
 		page.c_card_mp = get_data_value('card-main-panel')
