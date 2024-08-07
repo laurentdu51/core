76ef6484c750273dbf3e21041859bfa1fa45cd98
Mr Duhaz <admin@duhaz.fr>
2024-08-07 21:40:00 +0200

    add startup check

--- views.py
+++ views.py
@@ -53,60 +53,8 @@ def get_data_value(name):
 		data = Data.objects.get(d_titre_slugify = name)
 	except:
 		data = Data()
-		if name == "site-name":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "Duhaz Core"
-			data.save()
-		elif name == "site-logo":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "far fa-clone"
-			data.save()
-		elif name == "site-version":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "Jan. 2024"
-			data.save()
-		elif name == "background-color":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "#999"
-			data.save()
-		elif name == "background":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "background.jpeg"
-			data.save()
-		elif name == "background-logo":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "logo-txt-Mrduhaz.png"
-			data.save()
-		elif name == "login-menu":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "True"
-			data.save()
-		elif name == "includ-right-panel":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "None"
-			data.save()
-		elif name == "card-main-panel":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "True"
-			data.save()
-		elif name == "card-right-panel":
-			data.d_titre = name
-			data.d_type = "txt"
-			data.d_variable = "True"
-			data.save()
-		else :
-			data.d_variable = "Blop"
+		data.d_variable = "Blop"
 	return data.d_variable
-
 
 def update_data_value(name, value):
 	try:
