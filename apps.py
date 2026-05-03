11c9accac06f285b960e9902525d0c3bcde78463
Laurent Hazart <contact@duhaz.fr>
2026-05-03 12:25:24 +0200

    Fix Django 6 app init and clean .gitignore for Python cache files

--- apps.py
+++ apps.py
@@ -1,153 +1,62 @@
-import sys
 from django.apps import AppConfig
 
+
 class CoreConfig(AppConfig):
-	name = 'core'
+    name = 'core'
 
-	def ready(self):
+    def ready(self):
+        from django.db.models.signals import post_migrate
+        post_migrate.connect(self.create_default_site_content, sender=self)
 
-		if 'migrate' in sys.argv or 'makemigrations' in sys.argv:
-			return
+    def create_default_site_content(self, sender, **kwargs):
+        if kwargs.get('raw', False):
+            return
 
-		# Skip if database doesn't exist yet
-		from django.db import connection
-		try:
-			with connection.cursor() as cursor:
-				cursor.execute("SELECT 1 FROM core_data LIMIT 1")
-		except:
-			return
+        from django.db import connection
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT 1 FROM core_data LIMIT 1")
+        except Exception:
+            return
 
-		print("-- Démarage du Core --")
-		print(">> Vérification des variables d'environement")
+        from core.models import Data, Page
 
-		from core.models import Data
-		
-		print(">>> check site-name")
-		try :
-			data = Data.objects.get(d_titre_slugify = "site-name")
-		except:
-			data = Data()
-			data.d_titre = "site-name"
-			data.d_type = "txt"
-			data.d_variable = "Duhaz Core"
-			data.save()
-		print(">>> check site-logo")
-		try :
-			data = Data.objects.get(d_titre_slugify = "site-logo")
-		except:
-			data = Data()
-			data.d_titre = "site-logo"
-			data.d_type = "txt"
-			data.d_variable = "far fa-clone"
-			data.save()
+        defaults = [
+            ("site-name", "txt", "Duhaz Core"),
+            ("site-logo", "txt", "far fa-clone"),
+            ("site-version", "txt", "Jan. 2024"),
+            ("background-color", "txt", "#999"),
+            ("background", "txt", "background.jpeg"),
+            ("background-logo", "txt", "logo-txt-Mrduhaz.png"),
+            ("login-menu", "txt", "True"),
+            ("includ-right-panel", "txt", "None"),
+            ("card-main-panel", "txt", "True"),
+            ("card-right-panel", "txt", "True"),
+            ("rss-mode", "txt", "False"),
+        ]
 
-		print(">>> heck site-version")
-		try :
-			data = Data.objects.get(d_titre_slugify = "site-version")
-		except :
-			data = Data()
-			data.d_titre = "site-version"
-			data.d_type = "txt"
-			data.d_variable = "Jan. 2024"
-			data.save()
+        for title, d_type, value in defaults:
+            Data.objects.get_or_create(
+                d_titre=title,
+                defaults={
+                    'd_titre_slugify': title,
+                    'd_type': d_type,
+                    'd_variable': value,
+                },
+            )
 
-		print(">>> check background-color")
-		try :
-			data = Data.objects.get(d_titre_slugify = "background-color")
-		except :
-			data = Data()
-			data.d_titre = "background-color"
-			data.d_type = "txt"
-			data.d_variable = "#999"
-			data.save()
-
-		print(">>> check background")
-		try :
-			data = Data.objects.get(d_titre_slugify = "background")
-		except :
-			data = Data()
-			data.d_titre = "background"
-			data.d_type = "txt"
-			data.d_variable = "background.jpeg"
-			data.save()
-		
-		print(">>> check background-logo")
-		try :
-			data = Data.objects.get(d_titre_slugify = "background-logo")
-		except :
-			data = Data()
-			data.d_titre = "background-logo"
-			data.d_type = "txt"
-			data.d_variable = "logo-txt-Mrduhaz.png"
-			data.save()
-
-		print(">>> check login-menu")
-		try :
-			data = Data.objects.get(d_titre_slugify = "login-menu")
-		except :
-			data = Data()
-			data.d_titre = "login-menu"
-			data.d_type = "txt"
-			data.d_variable = "True"
-			data.save()
-
-		print(">>> check includ-right-panel")
-		try :
-			data = Data.objects.get(d_titre_slugify = "includ-right-panel")
-		except :
-			data = Data()
-			data.d_titre = "includ-right-panel"
-			data.d_type = "txt"
-			data.d_variable = "None"
-			data.save()
-
-		print(">>> check card-main-panel")
-		try :
-			data = Data.objects.get(d_titre_slugify = "card-main-panel")
-		except :
-			data = Data()
-			data.d_titre = "card-main-panel"
-			data.d_type = "txt"
-			data.d_variable = "True"
-			data.save()
-
-		print(">>> check card-right-panel")
-		try :
-			data = Data.objects.get(d_titre_slugify = "card-right-panel")
-		except :
-			data = Data()
-			data.d_titre = "card-right-panel"
-			data.d_type = "txt"
-			data.d_variable = "True"
-			data.save()
-
-		print(">>> check rss-mode")
-		try :
-			data = Data.objects.get(d_titre_slugify = "rss-mode")
-		except :
-			data = Data()
-			data.d_titre = "rss-mode"
-			data.d_type = "txt"
-			data.d_variable = "False"
-			data.save()
-
-		print(">> Vérification des pages par default")
-
-		from core.models import Page
-		print(">>> check bienvenus ")
-		try :
-			page = Page.objects.get(p_titre_slugify = "bienvenus")
-		except :
-			page = Page()
-			page.p_titre = "Bienvenus"
-			page.p_icone = "fas fa-home"
-			page.p_contenu = "Bravo,</br>Ceci est votre 1er page."
-			page.p_description = "Bravo, ceci est votre 1er page."
-			page.p_adresse = "/"
-			page.p_publier = True
-			page.p_type = "sys"
-
-			page.save()
+        Page.objects.get_or_create(
+            p_titre="Bienvenus",
+            defaults={
+                'p_titre_slugify': "bienvenus",
+                'p_icone': "fas fa-home",
+                'p_contenu': "Bravo,</br>Ceci est votre 1er page.",
+                'p_description': "Bravo, ceci est votre 1er page.",
+                'p_adresse': "/",
+                'p_publier': True,
+                'p_type': "sys",
+            },
+        )
 
 		
 
